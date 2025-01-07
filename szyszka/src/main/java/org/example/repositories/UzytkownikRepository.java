686d29ecce5f0131f6f7fa90c91a7e0ef9f512e3
kdemczyna <kdemczyna@gmail.com>
2025-01-07 18:21:45 +0100

    dodałem application.properties
    metody w controlerze powinny działac
    (mozna testowac za pomocą postmana)

--- szyszka/src/main/java/org/example/repositories/UzytkownikRepository.java
+++ szyszka/src/main/java/org/example/repositories/UzytkownikRepository.java
@@ -17,16 +17,8 @@ public interface UzytkownikRepository extends JpaRepository<Uzytkownik, Long> {
     Optional<Uzytkownik> findByLogin(String login);
 
 
-    List<Uzytkownik> findBySzostkaId(Long szostkaId);
-
-
     Optional<Uzytkownik> findByEmail(String email);
-
-
-    List<Uzytkownik> findByDataDolaczeniaDoGromadyAfter(java.time.LocalDateTime data);
-
 
     List<Uzytkownik> findByNazwiskoContaining(String fragmentNazwiska);
 
-    List<Uzytkownik> findByRodzic1IdOrRodzic2Id(Long rodzic1Id, Long rodzic2Id);
 }
